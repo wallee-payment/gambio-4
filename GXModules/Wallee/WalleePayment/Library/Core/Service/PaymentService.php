b066f7c27d0c6d50aef320f0276509b34942e0ce
root <root@runner-wuztpvdg-project-16868555-concurrent-08tvhr>
2023-09-21 15:20:08 +0200

    Release 1.0.14

--- GXModules/Wallee/WalleePayment/Library/Core/Service/PaymentService.php
+++ GXModules/Wallee/WalleePayment/Library/Core/Service/PaymentService.php
@@ -16,11 +16,11 @@ use ThemeId;
 use ThemeService;
 use ThemeSettings;
 use Wallee\Sdk\{Model\CreationEntityState,
-    Model\CriteriaOperator,
-    Model\EntityQuery,
-    Model\EntityQueryFilter,
-    Model\EntityQueryFilterType,
-    Model\PaymentMethodConfiguration
+  Model\CriteriaOperator,
+  Model\EntityQuery,
+  Model\EntityQueryFilter,
+  Model\EntityQueryFilterType,
+  Model\PaymentMethodConfiguration
 };
 use WalleeStorage;
 
@@ -31,155 +31,155 @@ use WalleeStorage;
  */
 class PaymentService
 {
-    /**
-     * @var string $rootDir
-     */
-    protected $rootDir;
-
-    /**
-     * @var Settings $settings
-     */
-    public $settings;
-
-    /**
-     * @var WalleeStorage $configuration
-     */
-    public $configuration;
-
-    /**
-     * @var LanguageTextManager $languageTextManager
-     */
-    public $languageTextManager;
-
-    /**
-     * @var array
-     */
-    private $localeLanguageMapping = [
-	'de-DE' => 'german',
-	'fr-FR' => 'french',
-	'it-IT' => 'italian',
-	'en-US' => 'english',
-    ];
-
-    /**
-     * PaymentService constructor.
-     * @param WalleeStorage|null $configuration
-     */
-    public function __construct(?WalleeStorage $configuration = null)
-    {
-	$this->rootDir = __DIR__ . '/../../../../../../';
-	$this->configuration = $configuration;
-	$this->settings = new Settings($this->configuration);
-	$this->languageTextManager = MainFactory::create_object(LanguageTextManager::class, array(), true);
-    }
-
-    public function syncPaymentMethods()
-    {
-	$paymentMethods = $this->getPaymentMethodConfigurations();
-
-	$translations = [];
-
-	$data = [];
 	/**
-	 * PaymentMethodConfiguration $paymentMethod
+	 * @var string $rootDir
 	 */
-	foreach ($paymentMethods as $paymentMethod) {
-	    $name = 'Wallee ' . $paymentMethod->getName();
-	    $slug = trim(strtolower(WalleeHelper::slugify($name)));
-
-	    $descriptions = [];
-	    $languageMapping = $this->localeLanguageMapping;
-	    foreach ($paymentMethod->getResolvedDescription() as $locale => $text) {
-		$language = $languageMapping[$locale];
-		$descriptions[$language] = $translations[$language][$slug . '_description'] = addslashes($text);
-	    }
-
-	    $titles = [];
-	    foreach ($paymentMethod->getResolvedTitle() as $locale => $text) {
-		$language = $languageMapping[$locale];
-		$titles[$language] = $translations[$language][$slug . '_title'] = addslashes(str_replace('-/', ' / ', $text));
-	    }
-
-	    $paymentMethodStateOnPortal = (string)$paymentMethod->getState();
-	    $data[] = [
-		'state' => $paymentMethodStateOnPortal,
-		'logo_url' => $paymentMethod->getResolvedImageUrl(),
-		'logo_alt' => $slug,
-		'id' => $slug,
-		'module' => $translations['english'][$slug . '_title'],
-		'description' => $translations['english'][$slug . '_description'],
-		'fields' => [],
-		'titles' => $titles,
-		'descriptions' => $descriptions
-	    ];
-
-	    $key = 'MODULE_PAYMENT_WALLEE_' . strtoupper($slug);
-	    $query = xtc_db_query("SELECT * FROM `gx_configurations` WHERE `key` = '" . xtc_db_input('configuration/' . $key) . "'");
-	    $result = xtc_db_fetch_array($query);
-
-	    if (empty($result)) {
-		$install_query = "insert into `gx_configurations` (`key`, `value`, `sort_order`, `type`, `last_modified`) "
-		    . "values ('configuration/" . $key . "', 'false', '0', 'switcher', now())";
-		xtc_db_query($install_query);
-
-		define($key . '_TITLE', $name . ' ' . $titles['english']);
-		define($key . '_DESC', $this->languageTextManager->get_text('would_you_like_to_enable_this_payment_method', 'wallee'));
-	    } else {
-		if ($result['state'] === 'true' && strtolower($paymentMethodStateOnPortal) === 'active') {
-		    xtc_db_perform(
-			'gx_configurations',
-			['value' => 'false'],
-			'update',
-			'key = ' . xtc_db_input('configuration/' . $key)
-		    );
+	protected $rootDir;
+	
+	/**
+	 * @var Settings $settings
+	 */
+	public $settings;
+	
+	/**
+	 * @var WalleeStorage $configuration
+	 */
+	public $configuration;
+	
+	/**
+	 * @var LanguageTextManager $languageTextManager
+	 */
+	public $languageTextManager;
+	
+	/**
+	 * @var array
+	 */
+	private $localeLanguageMapping = [
+	  'de-DE' => 'german',
+	  'fr-FR' => 'french',
+	  'it-IT' => 'italian',
+	  'en-US' => 'english',
+	];
+	
+	/**
+	 * PaymentService constructor.
+	 * @param WalleeStorage|null $configuration
+	 */
+	public function __construct(?WalleeStorage $configuration = null)
+	{
+		$this->rootDir = __DIR__ . '/../../../../../../';
+		$this->configuration = $configuration;
+		$this->settings = new Settings($this->configuration);
+		$this->languageTextManager = MainFactory::create_object(LanguageTextManager::class, array(), true);
+	}
+	
+	public function syncPaymentMethods()
+	{
+		$paymentMethods = $this->getPaymentMethodConfigurations();
+		
+		$translations = [];
+		
+		$data = [];
+		/**
+		 * PaymentMethodConfiguration $paymentMethod
+		 */
+		foreach ($paymentMethods as $paymentMethod) {
+			$name = 'Wallee ' . $paymentMethod->getName();
+			$slug = trim(strtolower(WalleeHelper::slugify($name)));
+			
+			$descriptions = [];
+			$languageMapping = $this->localeLanguageMapping;
+			foreach ($paymentMethod->getResolvedDescription() as $locale => $text) {
+				$language = $languageMapping[$locale];
+				$descriptions[$language] = $translations[$language][$slug . '_description'] = addslashes($text);
+			}
+			
+			$titles = [];
+			foreach ($paymentMethod->getResolvedTitle() as $locale => $text) {
+				$language = $languageMapping[$locale];
+				$titles[$language] = $translations[$language][$slug . '_title'] = addslashes(str_replace('-/', ' / ', $text));
+			}
+			
+			$paymentMethodStateOnPortal = (string)$paymentMethod->getState();
+			$data[] = [
+			  'state' => $paymentMethodStateOnPortal,
+			  'logo_url' => $paymentMethod->getResolvedImageUrl(),
+			  'logo_alt' => $slug,
+			  'id' => $slug,
+			  'module' => $translations['english'][$slug . '_title'],
+			  'description' => $translations['english'][$slug . '_description'],
+			  'fields' => [],
+			  'titles' => $titles,
+			  'descriptions' => $descriptions
+			];
+			
+			$key = 'MODULE_PAYMENT_WALLEE_' . strtoupper($slug);
+			$query = xtc_db_query("SELECT * FROM `gx_configurations` WHERE `key` = '" . xtc_db_input('configuration/' . $key) . "'");
+			$result = xtc_db_fetch_array($query);
+			
+			if (empty($result)) {
+				$install_query = "insert into `gx_configurations` (`key`, `value`, `sort_order`, `type`, `last_modified`) "
+				  . "values ('configuration/" . $key . "', 'false', '0', 'switcher', now())";
+				xtc_db_query($install_query);
+				
+				define($key . '_TITLE', $name . ' ' . $titles['english']);
+				define($key . '_DESC', $this->languageTextManager->get_text('would_you_like_to_enable_this_payment_method', 'wallee'));
+			} else {
+				if ($result['state'] === 'true' && strtolower($paymentMethodStateOnPortal) === 'active') {
+					xtc_db_perform(
+					  'gx_configurations',
+					  ['value' => 'false'],
+					  'update',
+					  'key = ' . xtc_db_input('configuration/' . $key)
+					);
+				}
+			}
 		}
-	    }
+		
+		$this->configuration->set('payment_methods', \json_encode($data));
 	}
-
-	$this->configuration->set('payment_methods', \json_encode($data));
-    }
-
-    /**
-     * @return mixed|WalleeStorage
-     */
-    public function getConfiguration()
-    {
-	return $this->configuration ?? MainFactory::create('WalleeStorage');
-    }
-
-    /**
-     * Fetch active merchant payment methods from Wallee API
-     *
-     * @return \Wallee\Sdk\Model\PaymentMethodConfiguration[]
-     * @throws \Wallee\Sdk\ApiException
-     * @throws \Wallee\Sdk\Http\ConnectionException
-     * @throws \Wallee\Sdk\VersioningException
-     */
-    private function getPaymentMethodConfigurations(): array
-    {
-	$entityQueryFilter = (new EntityQueryFilter())
-	    ->setOperator(CriteriaOperator::EQUALS)
-	    ->setFieldName('state')
-	    ->setType(EntityQueryFilterType::LEAF)
-	    ->setValue(CreationEntityState::ACTIVE);
-
-	$entityQuery = (new EntityQuery())->setFilter($entityQueryFilter);
-
-	$settings = new Settings($this->configuration);
-	$apiClient = $settings->getApiClient();
-	$spaceId = $settings->getSpaceId();
-
-	if (empty($spaceId)) {
-	    $GLOBALS['messageStack']->add_session($this->languageTextManager->get_text('no_payment_methods_were_imported_please_check_space_id_setting', 'wallee'), 'error');
-	    return [];
+	
+	/**
+	 * @return mixed|WalleeStorage
+	 */
+	public function getConfiguration()
+	{
+		return $this->configuration ?? MainFactory::create('WalleeStorage');
 	}
-
-	$paymentMethodConfigurations = $apiClient->getPaymentMethodConfigurationService()->search($spaceId, $entityQuery);
-
-	usort($paymentMethodConfigurations, function (PaymentMethodConfiguration $item1, PaymentMethodConfiguration $item2) {
-	    return $item1->getSortOrder() <=> $item2->getSortOrder();
-	});
-
-	return $paymentMethodConfigurations;
-    }
+	
+	/**
+	 * Fetch active merchant payment methods from Wallee API
+	 *
+	 * @return \Wallee\Sdk\Model\PaymentMethodConfiguration[]
+	 * @throws \Wallee\Sdk\ApiException
+	 * @throws \Wallee\Sdk\Http\ConnectionException
+	 * @throws \Wallee\Sdk\VersioningException
+	 */
+	public function getPaymentMethodConfigurations(): array
+	{
+		$entityQueryFilter = (new EntityQueryFilter())
+		  ->setOperator(CriteriaOperator::EQUALS)
+		  ->setFieldName('state')
+		  ->setType(EntityQueryFilterType::LEAF)
+		  ->setValue(CreationEntityState::ACTIVE);
+		
+		$entityQuery = (new EntityQuery())->setFilter($entityQueryFilter);
+		
+		$settings = new Settings($this->configuration);
+		$apiClient = $settings->getApiClient();
+		$spaceId = $settings->getSpaceId();
+		
+		if (empty($spaceId)) {
+			$GLOBALS['messageStack']->add_session($this->languageTextManager->get_text('no_payment_methods_were_imported_please_check_space_id_setting', 'wallee'), 'error');
+			return [];
+		}
+		
+		$paymentMethodConfigurations = $apiClient->getPaymentMethodConfigurationService()->search($spaceId, $entityQuery);
+		
+		usort($paymentMethodConfigurations, function (PaymentMethodConfiguration $item1, PaymentMethodConfiguration $item2) {
+			return $item1->getSortOrder() <=> $item2->getSortOrder();
+		});
+		
+		return $paymentMethodConfigurations;
+	}
 }
--- GXModules/Wallee/WalleePayment/Shop/Overloads/CheckoutProcessProcess/Wallee_CheckoutProcessProcess.inc.php
+++ GXModules/Wallee/WalleePayment/Shop/Overloads/CheckoutProcessProcess/Wallee_CheckoutProcessProcess.inc.php
@@ -1,8 +1,9 @@
 <?php
 
 use GXModules\WalleePayment\Library\{Core\Settings\Options\Integration,
-	Core\Settings\Struct\Settings,
-	Helper\WalleeHelper
+  Core\Settings\Struct\Settings,
+  Helper\WalleeHelper,
+  Core\Service\PaymentService
 };
 use Wallee\Sdk\Model\{AddressCreate, LineItemCreate, LineItemType, Transaction, TransactionCreate};
 
@@ -27,65 +28,69 @@ class Wallee_CheckoutProcessProcess extends Wallee_CheckoutProcessProcess_parent
 		if ($this->check_redirect()) {
 			return true;
 		}
-
+		
 		$this->_initOrderData();
-
+		
 		// check if tmp order id exists
 		if (!isset($_SESSION['tmp_oID']) || !is_int($_SESSION['tmp_oID'])) {
 			$this->save_order();
-
+			
 			$this->save_module_data();
 			$this->coo_order_total->apply_credit();
 			$this->process_products();
 			$this->save_tracking_data();
-
+			
 			// redirect to payment service
 			if ($this->tmp_order) {
 				$this->coo_payment->payment_action();
 			}
 		}
-
+		
 		if ($this->tmp_order === false) {
 			$settings = new Settings();
 			if (!$settings->isConfirmationEmailSendEnabled()) {
 				parent::send_order_mail();
 			}
-
+			
 			$this->coo_payment->after_process();
-
+			
 			$this->set_redirect_url(xtc_href_link("shop.php", 'do=WalleePayment/PaymentPage', 'SSL'));
 			return true;
 		}
-
+		
 		return true;
 	}
-
+	
 	/**
 	 * The save_order method stores the order and sets the orderId
 	 */
 	public function save_order()
 	{
+		if (strpos($_SESSION['payment'] ?? '', 'wallee') === false) {
+			return parent::save_order();
+		}
+		
 		$settings = new Settings();
 		$integration = $settings->getIntegration();
-
+		
 		$orderId = $this->createOrder();
-		$createdTransaction = $this->createRemoteTransaction($orderId, $settings);
-
+		$createdTransaction = $this->createRemoteTransaction($orderId, $settings, $integration);
+		
 		$createdTransactionId = $createdTransaction->getId();
-
+		
 		$this->confirmTransaction($createdTransaction);
-
+		
 		$transactionModel = new WalleeTransactionModel();
 		$transactionModel->create($settings, $createdTransactionId, $orderId, (array)$this->coo_order);
-
+		
 		$_SESSION['integration'] = $integration;
 		$_SESSION['transactionID'] = $createdTransactionId;
 		$this->_setOrderId($orderId);
-
+		
 		if ($integration == Integration::PAYMENT_PAGE) {
 			$redirectUrl = $settings->getApiClient()->getTransactionPaymentPageService()
-				->paymentPageUrl($settings->getSpaceId(), $createdTransactionId);
-
+			  ->paymentPageUrl($settings->getSpaceId(), $createdTransactionId);
+			
 			xtc_redirect($redirectUrl);
 			return;
 		}
@@ -93,62 +98,63 @@ class Wallee_CheckoutProcessProcess extends Wallee_CheckoutProcessProcess_parent
 		$_SESSION['possiblePaymentMethod'] = $this->getTransactionPaymentMethod($settings, $createdTransactionId);
 		$_SESSION['orderTotal'] = $this->coo_order_total->output_array();
 	}
-
-    /**
-     * @param Transaction $transaction
-     * @throws \Wallee\Sdk\ApiException
-     * @throws \Wallee\Sdk\Http\ConnectionException
-     * @throws \Wallee\Sdk\VersioningException
-     */
-	private function confirmTransaction(Transaction $transaction): void {
-	    $pendingTransaction = new TransactionPending();
-	    $pendingTransaction->setId($transaction->getId());
-	    $pendingTransaction->setVersion($transaction->getVersion());
-
-	    $settings = new Settings();
-	    $settings->getApiClient()->getTransactionService()
-		->confirm($settings->getSpaceId(), $pendingTransaction);
+	
+	/**
+	 * @param Transaction $transaction
+	 * @throws \Wallee\Sdk\ApiException
+	 * @throws \Wallee\Sdk\Http\ConnectionException
+	 * @throws \Wallee\Sdk\VersioningException
+	 */
+	private function confirmTransaction(Transaction $transaction): void
+	{
+		$pendingTransaction = new TransactionPending();
+		$pendingTransaction->setId($transaction->getId());
+		$pendingTransaction->setVersion($transaction->getVersion());
+		
+		$settings = new Settings();
+		$settings->getApiClient()->getTransactionService()
+		  ->confirm($settings->getSpaceId(), $pendingTransaction);
 	}
-
+	
 	/**
 	 * @return string
 	 */
 	private function createOrder(): string
 	{
 		return $this->orderWriteService->createNewCustomerOrder($this->_getCustomerId(),
-			$this->_getCustomerStatusInformation(),
-			$this->_getCustomerNumber(),
-			$this->_getCustomerEmail(),
-			$this->_getCustomerTelephone(),
-			$this->_getCustomerVatId(),
-			$this->_getCustomerDefaultAddress(),
-			$this->_getBillingAddress(),
-			$this->_getDeliveryAddress(),
-			$this->_getOrderItemCollection(),
-			$this->_getOrderTotalCollection(),
-			$this->_getOrderShippingType(),
-			$this->_getOrderPaymentType(),
-			$this->_getCurrencyCode(),
-			$this->_getLanguageCode(),
-			$this->_getOrderTotalWeight(),
-			$this->_getComment(),
-			$this->_getOrderStatusId(),
-			$this->_getOrderAddonValuesCollection());
+		  $this->_getCustomerStatusInformation(),
+		  $this->_getCustomerNumber(),
+		  $this->_getCustomerEmail(),
+		  $this->_getCustomerTelephone(),
+		  $this->_getCustomerVatId(),
+		  $this->_getCustomerDefaultAddress(),
+		  $this->_getBillingAddress(),
+		  $this->_getDeliveryAddress(),
+		  $this->_getOrderItemCollection(),
+		  $this->_getOrderTotalCollection(),
+		  $this->_getOrderShippingType(),
+		  $this->_getOrderPaymentType(),
+		  $this->_getCurrencyCode(),
+		  $this->_getLanguageCode(),
+		  $this->_getOrderTotalWeight(),
+		  $this->_getComment(),
+		  $this->_getOrderStatusId(),
+		  $this->_getOrderAddonValuesCollection());
 	}
-
-    /**
-     * @param string $orderId
-     * @param Settings $settings
-     * @throws \Wallee\Sdk\ApiException
-     * @throws \Wallee\Sdk\Http\ConnectionException
-     * @throws \Wallee\Sdk\VersioningException
-     * @return Transaction
-     */
-	private function createRemoteTransaction(string $orderId, Settings $settings): Transaction
+	
+	/**
+	 * @param string $orderId
+	 * @param Settings $settings
+	 * @param string $integration
+	 * @return Transaction
+	 * @throws \Wallee\Sdk\ApiException
+	 * @throws \Wallee\Sdk\Http\ConnectionException
+	 * @throws \Wallee\Sdk\VersioningException
+	 */
+	private function createRemoteTransaction(string $orderId, Settings $settings, string $integration): Transaction
 	{
-
+		
 		$order = (array)$this->coo_order;
-
 		$lineItems = [];
 		foreach ($order['products'] as $product) {
 			$lineItem = new LineItemCreate();
@@ -172,31 +178,10 @@ class Wallee_CheckoutProcessProcess extends Wallee_CheckoutProcessProcess_parent
 			$lineItem->setType(LineItemType::SHIPPING);
 			$lineItems[] = $lineItem;
 		}
-
-		$billingAddress = new AddressCreate();
-		$billingAddress->setCity($order['billing']['city']);
-		$billingAddress->setCountry($order['billing']['country']['iso_code_2']);
-		$billingAddress->setEmailAddress($order['customer']['email_address']);
-		$billingAddress->setFamilyName($order['billing']['lastname']);
-		$billingAddress->setGivenName($order['billing']['firstname']);
-		$billingAddress->setPostCode($order['billing']['postcode']);
-		$billingAddress->setPostalState($order['billing']['state']);
-		$billingAddress->setOrganizationName($order['billing']['company']);
-		$billingAddress->setPhoneNumber($order['customer']['telephone']);
-		$billingAddress->setSalutation($order['customer']['gender'] === 'm' ? 'Mr' : 'Ms');
-
-		$shippingAddress = new AddressCreate();
-		$shippingAddress->setCity($order['delivery']['city']);
-		$shippingAddress->setCountry($order['delivery']['country']['iso_code_2']);
-		$shippingAddress->setEmailAddress($order['customer']['email_address']);
-		$shippingAddress->setFamilyName($order['delivery']['lastname']);
-		$shippingAddress->setGivenName($order['delivery']['firstname']);
-		$shippingAddress->setPostCode($order['delivery']['postcode']);
-		$shippingAddress->setPostalState($order['delivery']['state']);
-		$shippingAddress->setOrganizationName($order['delivery']['company']);
-		$shippingAddress->setPhoneNumber($order['customer']['telephone']);
-		$shippingAddress->setSalutation($order['customer']['gender'] === 'm' ? 'Mr' : 'Ms');
-
+		
+		$billingAddress = $this->getBillingAddress($order);
+		$shippingAddress = $this->getShippingAddress($order);
+		
 		$transactionPayload = new TransactionCreate();
 		$transactionPayload->setCurrency($order['info']['currency']);
 		$transactionPayload->setLineItems($lineItems);
@@ -204,19 +189,42 @@ class Wallee_CheckoutProcessProcess extends Wallee_CheckoutProcessProcess_parent
 		$transactionPayload->setShippingAddress($shippingAddress);
 		$transactionPayload->setMerchantReference($orderId);
 		$transactionPayload->setMetaData([
-			'orderId' => $orderId,
-			'spaceId' => $settings->getSpaceId(),
+		  'orderId' => $orderId,
+		  'spaceId' => $settings->getSpaceId(),
 		]);
 		$transactionPayload->setSpaceViewId($settings->getSpaceViewId());
 		$transactionPayload->setAutoConfirmationEnabled(getenv('WALLEE_AUTOCONFIRMATION_ENABLED') ?: false);
-
+		
+		if ($integration === Integration::PAYMENT_PAGE) {
+			$paymentMethodConfigurationId = $this->getPaymentMethodConfigurationId();
+			if ($paymentMethodConfigurationId) {
+				$transactionPayload->setAllowedPaymentMethodConfigurations([$paymentMethodConfigurationId]);
+			}
+		}
+		
 		$transactionPayload->setSuccessUrl(xtc_href_link(FILENAME_CHECKOUT_SUCCESS, '', 'SSL'));
 		$transactionPayload->setFailedUrl(xtc_href_link(FILENAME_CHECKOUT_PAYMENT . '?payment_error', '', 'SSL'));
 		$createdTransaction = $settings->getApiClient()->getTransactionService()->create($settings->getSpaceId(), $transactionPayload);
-
+		
 		return $createdTransaction;
 	}
-
+	
+	private function getPaymentMethodConfigurationId()
+	{
+		$paymentMethodConfigurationId = null;
+		$paymentService = new PaymentService(MainFactory::create('WalleeStorage'));
+		$paymentMethodConfigurations = $paymentService->getPaymentMethodConfigurations();
+		foreach ($paymentMethodConfigurations as $paymentMethodConfiguration) {
+			$slug = 'wallee_' . trim(strtolower(WalleeHelper::slugify($paymentMethodConfiguration->getName())));
+			if ($_SESSION['choosen_payment_method'] === $slug) {
+				$paymentMethodConfigurationId = $paymentMethodConfiguration->getId();
+				break;
+			}
+		}
+		
+		return $paymentMethodConfigurationId;
+	}
+	
 	/**
 	 * @param Settings $settings
 	 * @param string $transactionId
@@ -228,21 +236,57 @@ class Wallee_CheckoutProcessProcess extends Wallee_CheckoutProcessProcess_parent
 	private function getTransactionPaymentMethod(Settings $settings, string $transactionId): array
 	{
 		$possiblePaymentMethods = $settings->getApiClient()
-			->getTransactionService()
-			->fetchPaymentMethods(
-				$settings->getSpaceId(),
-				$transactionId,
-				$settings->getIntegration()
-			);
-
+		  ->getTransactionService()
+		  ->fetchPaymentMethods(
+			$settings->getSpaceId(),
+			$transactionId,
+			$settings->getIntegration()
+		  );
+		
 		$chosenPaymentMethod = $_SESSION['choosen_payment_method'];
-
+		
 		return array_filter($possiblePaymentMethods, function ($possiblePaymentMethod) use ($chosenPaymentMethod) {
-				$slug = 'wallee_' . trim(strtolower(WalleeHelper::slugify($possiblePaymentMethod->getName())));
-				return $slug === $chosenPaymentMethod;
-			}) ?? [];
+			$slug = 'wallee_' . trim(strtolower(WalleeHelper::slugify($possiblePaymentMethod->getName())));
+			return $slug === $chosenPaymentMethod;
+		}) ?? [];
 	}
-
+	
+	private function getBillingAddress($order): AddressCreate
+	{
+		$billingAddress = new AddressCreate();
+		$billingAddress->setStreet($order['billing']['street_address']);
+		$billingAddress->setCity($order['billing']['city']);
+		$billingAddress->setCountry($order['billing']['country']['iso_code_2']);
+		$billingAddress->setEmailAddress($order['customer']['email_address']);
+		$billingAddress->setFamilyName($order['billing']['lastname']);
+		$billingAddress->setGivenName($order['billing']['firstname']);
+		$billingAddress->setPostCode($order['billing']['postcode']);
+		$billingAddress->setPostalState($order['billing']['state']);
+		$billingAddress->setOrganizationName($order['billing']['company']);
+		$billingAddress->setPhoneNumber($order['customer']['telephone']);
+		$billingAddress->setSalutation($order['customer']['gender'] === 'm' ? 'Mr' : 'Ms');
+		
+		return $billingAddress;
+	}
+	
+	private function getShippingAddress($order): AddressCreate
+	{
+		$shippingAddress = new AddressCreate();
+		$shippingAddress->setStreet($order['delivery']['street_address']);
+		$shippingAddress->setCity($order['delivery']['city']);
+		$shippingAddress->setCountry($order['delivery']['country']['iso_code_2']);
+		$shippingAddress->setEmailAddress($order['customer']['email_address']);
+		$shippingAddress->setFamilyName($order['delivery']['lastname']);
+		$shippingAddress->setGivenName($order['delivery']['firstname']);
+		$shippingAddress->setPostCode($order['delivery']['postcode']);
+		$shippingAddress->setPostalState($order['delivery']['state']);
+		$shippingAddress->setOrganizationName($order['delivery']['company']);
+		$shippingAddress->setPhoneNumber($order['customer']['telephone']);
+		$shippingAddress->setSalutation($order['customer']['gender'] === 'm' ? 'Mr' : 'Ms');
+		
+		return $shippingAddress;
+	}
+	
 	/**
 	 * @param int $transactionId
 	 * @return string
@@ -253,8 +297,8 @@ class Wallee_CheckoutProcessProcess extends Wallee_CheckoutProcessProcess_parent
 	private function getTransactionJavaScriptUrl(int $transactionId): string
 	{
 		$settings = new Settings();
-
+		
 		return $settings->getApiClient()->getTransactionIframeService()
-			->javascriptUrl($settings->getSpaceId(), $transactionId);
+		  ->javascriptUrl($settings->getSpaceId(), $transactionId);
 	}
 }

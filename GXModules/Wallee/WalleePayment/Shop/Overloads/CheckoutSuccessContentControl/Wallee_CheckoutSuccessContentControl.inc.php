11fbb61ea338282131a29e8db3d966634b779b22
root <root@runner-auhfcl5gx-project-174-concurrent-0-mtx3b54r>
2024-09-03 16:15:56 +0200

    Release 1.0.24

--- GXModules/Wallee/WalleePayment/Shop/Overloads/CheckoutSuccessContentControl/Wallee_CheckoutSuccessContentControl.inc.php
+++ GXModules/Wallee/WalleePayment/Shop/Overloads/CheckoutSuccessContentControl/Wallee_CheckoutSuccessContentControl.inc.php
@@ -2,34 +2,33 @@
 
 class Wallee_CheckoutSuccessContentControl extends Wallee_CheckoutSuccessContentControl_parent
 {
-	public function proceed()
+	public function proceed(): void
 	{
 		if (strpos($_SESSION['payment'] ?? '', 'wallee') !== false) {
 			$coo_checkout_process = MainFactory::create_object('CheckoutProcessProcess');
 			$coo_checkout_process->set_data('GET', $_GET);
 			$coo_checkout_process->set_data('POST', $_POST);
 			$coo_checkout_process->set_data('GLOBALS', $GLOBALS);
-			
+
 			$coo_checkout_process->set_('coo_order_total', new order_total());
 			$coo_checkout_process->set_('coo_properties', MainFactory::create_object('PropertiesControl'));
 			$coo_checkout_process->set_('coo_order', $_SESSION['global_order']);
 			$coo_checkout_process->process_products();
-			
+
 			$this->reset();
 		}
 
 		if ($_SESSION['order_id'] !== null && $_SESSION['email_sent_' . $_SESSION['order_id']] !== true) {
 		    $this->sendEmail();
 		}
-		
+
 		parent::proceed();
-		return true;
 	}
-	
+
 	public function reset()
 	{
 		$_SESSION['cart']->reset(true);
-		
+
 		// unregister session variables used during checkout
 		unset($_SESSION['sendto']);
 		unset($_SESSION['billto']);
@@ -42,7 +41,7 @@ class Wallee_CheckoutSuccessContentControl extends Wallee_CheckoutSuccessContent
 		unset($_SESSION['nvpReqArray']);
 		unset($_SESSION['reshash']);
 		$GLOBALS['last_order'] = $this->order_id;
-		
+
 		//GV Code Start
 		if (isset($_SESSION['credit_covers'])) {
 			unset($_SESSION['credit_covers']);
@@ -54,7 +53,7 @@ class Wallee_CheckoutSuccessContentControl extends Wallee_CheckoutSuccessContent
 		unset($_SESSION['javascriptUrl']);
 		unset($_SESSION['integration']);
 	}
-	
+
 	protected function sendEmail()
 	{
 	    $coo_send_order_process = MainFactory::create_object('SendOrderProcess');
@@ -62,5 +61,5 @@ class Wallee_CheckoutSuccessContentControl extends Wallee_CheckoutSuccessContent
 	    $coo_send_order_process->proceed();
 	    $_SESSION['email_sent_' . $_SESSION['order_id']] = true;
 	}
- 
+
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-Wallee Payment for Gambio 4.8
+Wallee Payment for Gambio 4
 =============================
 
 The Wallee Payment plugin wraps around the Wallee API. This library facilitates your interaction with various services such as transactions.
@@ -8,7 +8,7 @@ The Wallee Payment plugin wraps around the Wallee API. This library facilitates 
 ## Requirements
 
 - PHP 7.2 to PHP 8.2
-- Gambio 4.5 to Gambio 4.8
+- Gambio 4.5 to Gambio 4.9
 
 We only support the Gambio standard checkout (without modifications)
 
@@ -57,7 +57,7 @@ account dashboard.
 
 ## Documentation
 
-[Documentation](https://plugin-documentation.wallee.com/wallee-payment/gambio-4/1.0.23/docs/en/documentation.html)
+[Documentation](https://plugin-documentation.wallee.com/wallee-payment/gambio-4/1.0.24/docs/en/documentation.html)
 
 ## License
 
